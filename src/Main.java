73300a83aa729bfe38a4445a6db710272a2b8fea
RichardNooooh <richard.jun.noh@gmail.com>
2020-02-14 19:58:17 -0600

    Add ASCII art .txt format
    Resulting image from ASCII image conversion is now placed in a .txt file
    instead of stdout using BufferedWriter.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.*;
 import java.io.*;
-import java.util.Arrays;
+
 
 public class Main
 {
@@ -22,29 +22,34 @@ public class Main
 		{
 			File input = new File("src/Images/weeb.jpg");
 			image = ImageIO.read(input);
-		} catch (Exception e) {}
 
-		//iterate through each pixel and calculate its brightness
-		int width = image.getWidth();
-		int height = image.getHeight();
+			//iterate through each pixel and calculate its brightness
+			int width = image.getWidth();
+			int height = image.getHeight();
 
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
+			File output = new File ("src/Images/ASCIIweeb.txt");
+			BufferedWriter writer = new BufferedWriter(new FileWriter(output));
+			for (int x = 0; x < width; x++)
 			{
-				Color pixel = new Color(image.getRGB(y, x));
-				int r = pixel.getRed();
-				int g = pixel.getBlue();
-				int b = pixel.getBlue();
-				//HSP Color Model: sqrt(0.299 * R^2 + 0.587 * G^2 + 0.114 * B^2)
-				double brightness = Math.sqrt(0.299 * r * r + 0.587 * g * g + 0.114 * b * b);
-				String ascii = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"; //70 long-
-				//scale factor: 70/256
-				int ind = (int)(brightness * 70 / 256);
-				System.out.print(ascii.charAt(ind)); //TODO edit scaling to have it somewhat nonlinear, to balance the uneven font
+				StringBuilder builder = new StringBuilder();
+				for (int y = 0; y < height; y++)
+				{
+					Color pixel = new Color(image.getRGB(y, x));
+					int r = pixel.getRed();
+					int g = pixel.getBlue();
+					int b = pixel.getBlue();
+					//HSP Color Model: sqrt(0.299 * R^2 + 0.587 * G^2 + 0.114 * B^2)
+					double brightness = Math.sqrt(0.299 * r * r + 0.587 * g * g + 0.114 * b * b);
+					String ascii = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"; //70 long-
+					//scale factor: 70/256
+					//TODO edit scaling to have it somewhat nonlinear, to balance the uneven font
+					int ind = (int)(brightness * 70 / 256);
+					builder.append(ascii.charAt(ind));
+				}
+				writer.write(builder.toString() + "\n");
 			}
-			System.out.println();
-		}
+			writer.close();
+		} catch (Exception e) {}
 
 	}
 
