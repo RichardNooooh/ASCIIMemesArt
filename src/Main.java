ce11c627737a66b441712bafd410699e9e3eb9f2
RichardNooooh <richard.jun.noh@gmail.com>
2020-02-14 20:09:26 -0600

    Remove printRGBArray() method
    Removed the debugging method, printRGBArray() as it is no longer needed
    at this point in development.

--- src/Main.java
+++ src/Main.java
@@ -53,38 +53,4 @@ public class Main
 
 	}
 
-	private static void printRGBArray(BufferedImage image)
-	{
-		//iterate through pixels
-		int width = image.getWidth();
-		int height = image.getHeight();
-		int[][][] rgbArray = new int[width][height][3]; //storage is unnecessary
-
-		for (int x = 0; x < width; x++)
-		{
-			for (int y = 0; y < height; y++)
-			{
-				Color currPixel = new Color(image.getRGB(x,y));
-				rgbArray[x][y][0] = currPixel.getRed();
-				rgbArray[x][y][1] = currPixel.getGreen();
-				rgbArray[x][y][2] = currPixel.getBlue();
-			}
-		}
-		//printout 3d array
-		for (int k = 0; k < 3; k++)
-		{
-			System.out.println(k + "th layer:");
-			for (int i = 0; i < width; i++)
-			{
-				System.out.print("[");
-				for (int j = 0; j < height; j++)
-				{
-					System.out.printf("  %3d", rgbArray[i][j][k]);
-				}
-				System.out.println("  ]");
-			}
-			System.out.println("\n");
-		}
-	}
-
 }
