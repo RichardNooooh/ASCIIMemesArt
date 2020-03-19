8ae506d1e8954b4dbea19331f8c6f24e454ed3bf
Ice <richard.jun.noh@gmail.com>
2020-03-18 20:30:01 -0500

    Add pixel reading with PIL module

--- __main__.py
+++ __main__.py
@@ -1,4 +1,11 @@
-import cv2
+from PIL import Image
 
+im = Image.open("images/weeb.jpg")
+pixel = im.load()
 
+width, height = im.size
 
+for x in range(width):
+    for y in range(height):
+        print(str(pixel[x, y]) + " ", end="")
+    print()
