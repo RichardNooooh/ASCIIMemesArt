26fc75c3703e0faf7d72b411100f0faa5fce2d8e
Ice <richard.jun.noh@gmail.com>
2020-03-19 23:32:50 -0500

    Add image to text linear mapping in Python
    The state of this commit is similar to:
    1dc405e3cc235664ae909f524ddac322875c0c60

--- __main__.py
+++ __main__.py
@@ -1,3 +1,5 @@
+import math
+
 from PIL import Image
 
 im = Image.open("images/weeb.jpg")
@@ -5,7 +7,17 @@ pixel = im.load()
 
 width, height = im.size
 
+output_string = ""
+
 for x in range(width):
     for y in range(height):
-        print(str(pixel[x, y]) + " ", end="")
-    print()
+        r, g, b = pixel[y, x]
+        brightness = math.sqrt(0.299 * r **2 + 0.587 * g ** 2 + 0.114 * b ** 2)
+        asciiMap = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+        ind = brightness * 70 // 256
+        output_string += asciiMap[int(ind)]
+
+    output_string += "\n"
+
+print(output_string)
+
