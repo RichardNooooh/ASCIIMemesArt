d2708d20602ab48d848a64946d2fd25de960a47c
Ice <richard.jun.noh@gmail.com>
2020-03-29 22:50:46 -0500

    Add start of font-sort
    This commit adds a new font to manipulate with, along with ImageDraw and
    ImageMath. This is just the start; no new features.

--- font_sort.py
+++ font_sort.py
@@ -1,3 +1,18 @@
 from PIL import Image
-from PIL import ImageFont
+from PIL import ImageFont, ImageDraw
+from PIL import ImageMath
 
+font = ImageFont.truetype("Open_Sans/OpenSans-Regular.ttf")
+mask = font.getmask('a')
+# draw = ImageDraw.Draw()
+
+size = 50, 50
+blank = Image.new('L', size, 256)  # not sure what value is needed for pure white
+blank.save("images/test.png")
+
+#
+# s = 0
+# ImageMath.eval("sum += int(a), 'L'", sum = s, a = mask)
+
+# print(mask)
+# mask.save("images/a.jpg")
