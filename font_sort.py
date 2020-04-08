5a3a7c4cc54fd47985e419626b7c456a3fbc95e4
Ice <richard.jun.noh@gmail.com>
2020-04-08 16:50:39 -0500

    Add density sort for each alphabetical character
    The current implementation appears to be pretty inefficient due to the
    fact that the getpixel() might be really inefficient based on the
    documentation. I've since given up on trying to use the ImageMath module
    to more efficiently count the number of black pixels in the glyph image.
    It appears that the evaluation method runs in a completely separate
    python environment and it doesn't seem possible to pass in a non-Image
    variable into the method.
    
    Until I learn more about PIL, or until such functionality is
    implemented, this crude method is the only viable solution I have,
    unless I frankenstein some stuff with another image library like OpenCV
    (if I can manage the get that working in my Pycharm IDE). Future commits
    will implement a way to properly crop the glyph onto the image canvas,
    and keeping that standardized.

--- font_sort.py
+++ font_sort.py
@@ -1,4 +1,6 @@
-from PIL import Image, ImageFont, ImageDraw
+from typing import Tuple
+
+from PIL import Image, ImageFont, ImageDraw, ImageMath
 import string
 
 
@@ -23,18 +25,28 @@ import string
 # canvas.save("images/test.png", "PNG")
 # canvas.show()
 
+# each pixel should be either completely black or white, so I just need to count the black pixels
+def density_of_letter(im: Image, size: Tuple[int]) -> float:
+    x, y = size
+    black_pixel_count = 0
+    for i in range(x):
+        for j in range(y):
+            pixel = im.getpixel((i, j))
+            if 0 in pixel:
+                black_pixel_count += 1
 
-def density_of_letter(im: Image) -> float:
-    
-    pass
+    return black_pixel_count / (x * y)
 
 
 font = ImageFont.truetype("Open_Sans/OpenSans-Regular.ttf", 35, encoding="unic")
+size = (50, 50)
 
 for letter in string.ascii_lowercase:
     # text_width, text_height = font.getsize(letter)
-    canvas_letter = Image.new('RGB', (100, 100), "white")
+    canvas_letter = Image.new('RGB', size, "white")
     draw_letter = ImageDraw.Draw(canvas_letter)
-    draw_letter.text((50, 50), letter, 'black', font)
-    # canvas_letter.save("images/test" + letter + ".png", "PNG")
-    dens = density_of_letter(canvas_letter)
+    draw_letter.text((0, 0), letter, 'black', font)
+    dens = density_of_letter(canvas_letter, size)
+    print(letter + " : " + str(dens))
+
+canvas_letter.show()
