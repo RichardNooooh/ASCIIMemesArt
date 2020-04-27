09b98134d1798dd19be5dd2ed3c4ddb45757c2b1
Ice <richard.jun.noh@gmail.com>
2020-04-27 16:30:44 -0500

    Refactor the font sorter into a class

--- font_sort.py
+++ font_sort.py
@@ -1,30 +1,8 @@
 from typing import Tuple
 
-from PIL import Image, ImageFont, ImageDraw, ImageMath
+from PIL import Image, ImageFont, ImageDraw
 import string
 
-
-# from https://stackoverflow.com/questions/24085996/how-i-can-load-a-font-file-with-pil-imagefont-truetype-without-specifying-the-ab
-
-# sample text and font
-# unicode_text = u"t"
-# font = ImageFont.truetype("Open_Sans/OpenSans-Regular.ttf", 28, encoding="unic")
-
-# get the line size
-# text_width, text_height = font.getsize(unicode_text)
-
-# create a blank canvas with extra space between lines
-# canvas = Image.new('RGB', (text_width, text_height+1), "white")
-
-# draw the text onto the text canvas, and use black as the text color
-# draw = ImageDraw.Draw(canvas)
-# draw.text((0, 0), unicode_text, 'black', font)
-
-# save the blank canvas to a file
-# canvas.save("images/test.png", "PNG")
-# canvas.show()
-
-# each pixel should be either completely black or white, so I just need to count the black pixels
 def density_of_letter(im: Image, size: Tuple[int]) -> float:
     x, y = size
     black_pixel_count = 0
@@ -37,15 +15,22 @@ def density_of_letter(im: Image, size: Tuple[int]) -> float:
     return black_pixel_count / (x * y)
 
 
-font = ImageFont.truetype("Open_Sans/OpenSans-Regular.ttf", 35, encoding="unic")
-size = (50, 50)
+class FontSort:
 
-for letter in string.ascii_lowercase:
-    # text_width, text_height = font.getsize(letter)
-    canvas_letter = Image.new('L', size, "white")
-    draw_letter = ImageDraw.Draw(canvas_letter)
-    draw_letter.text((0, 0), letter, 'black', font)
-    dens = density_of_letter(canvas_letter, size)
-    print(letter + " : " + str(dens))
+    def __init__(self, font_filename: str):
+        self.font_filename = font_filename
+        self.font_dict = {}
 
-canvas_letter.show()
+    def find_densities(self):
+        font = ImageFont.truetype(self.font_filename, 35, encoding="unic")
+        size = (50, 50)
+
+        for letter in string.ascii_lowercase:
+            canvas_letter = Image.new('L', size, "white")
+            draw_letter = ImageDraw.Draw(canvas_letter)
+            draw_letter.text((0, 0), letter, 'black', font)
+            dens = density_of_letter(canvas_letter, size)
+            self.font_dict[letter] = dens
+
+    def get_font_dict(self):
+        return self.font_dict
