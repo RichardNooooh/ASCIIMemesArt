90872733e2b9d7b26ef3acb603f2926b72fbb15f
Ice <richard.jun.noh@gmail.com>
2020-04-28 18:58:54 -0500

    Add connection between font_sort and main
    I've also added a few new images to the images directory.

--- __main__.py
+++ __main__.py
@@ -1,4 +1,5 @@
 import math
+from font_sort import FontSort
 
 from PIL import Image
 
@@ -9,13 +10,24 @@ width, height = im.size
 
 output_string = ""
 
-for x in range(width):
-    for y in range(height):
-        r, g, b = pixel[y, x]
-        brightness = math.sqrt(0.299 * r **2 + 0.587 * g ** 2 + 0.114 * b ** 2)
-        asciiMap = " .'`^\",:;Il!i><~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
-        ind = brightness * 70 // 256
-        output_string += asciiMap[int(ind)]
+mapping_string = " .'`^\",:;Il!i~+_-?1|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$"
+ft = FontSort("Open_Sans/OpenSans-Regular.ttf", mapping_string)
+asciiMap = ft.get_reversed_font_dict()
+
+for y in range(height):
+    for x in range(width):
+        pixel_tuple = pixel[x, y]
+        if len(pixel_tuple) > 3:
+            r, g, b, a = pixel_tuple
+            r *= a / 255
+            g *= a / 255
+            b *= a / 255
+        else:
+            r, g, b = pixel_tuple
+
+        brightness = math.sqrt(0.299 * r ** 2 + 0.587 * g ** 2 + 0.114 * b ** 2)
+        ind = brightness * len(mapping_string) // 256
+        output_string += asciiMap[int(ind)][0]
 
     output_string += "\n"
 
