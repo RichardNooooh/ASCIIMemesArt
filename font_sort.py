8c58da1e6c4819f7f51df0ce0bd1548d98a55350
Ice <richard.jun.noh@gmail.com>
2020-04-28 18:58:54 -0500

    Add connection between font_sort and main
    I've also added a few new images to the images directory.
    After experimenting with the image conversions, I've since realized that
    most images can only be recognized if viewed from far away. So, I will
    be adding a new feature where the text is printed onto a new image file
    and outputted somewhere.

--- font_sort.py
+++ font_sort.py
@@ -3,6 +3,7 @@ from typing import Tuple
 from PIL import Image, ImageFont, ImageDraw
 import string
 
+
 def density_of_letter(im: Image, size: Tuple[int]) -> float:
     x, y = size
     black_pixel_count = 0
@@ -17,20 +18,27 @@ def density_of_letter(im: Image, size: Tuple[int]) -> float:
 
 class FontSort:
 
-    def __init__(self, font_filename: str):
+    def __init__(self, font_filename: str, unsorted_letters: str):
         self.font_filename = font_filename
-        self.font_dict = {}
+        self.unsorted_letters = unsorted_letters
+        self.font_dict = []
+        self.find_densities()
 
     def find_densities(self):
         font = ImageFont.truetype(self.font_filename, 35, encoding="unic")
         size = (50, 50)
 
-        for letter in string.ascii_lowercase:
+        for letter in self.unsorted_letters:
             canvas_letter = Image.new('L', size, "white")
             draw_letter = ImageDraw.Draw(canvas_letter)
             draw_letter.text((0, 0), letter, 'black', font)
             dens = density_of_letter(canvas_letter, size)
-            self.font_dict[letter] = dens
+            self.font_dict.append((letter, dens))
+
+        self.font_dict.sort(key=lambda word: word[1])
 
     def get_font_dict(self):
-        return self.font_dict
+        return list(self.font_dict)
+
+    def get_reversed_font_dict(self):
+        return list(reversed(self.font_dict))
